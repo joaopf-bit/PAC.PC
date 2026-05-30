551f11cb0fbb76b5947aef6456505bcae4ddf919
TuJPan <joao.pf@estudante.iftm.edu.br>
2026-05-30 18:26:27 -0300

    Fix: Adicionando jsconfig.json e devDependencies para mapeamento de rotas

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-// Certifique-se de que está escrito exatamente assim, sem chaves na exportação
-export default nextConfig;
+{
+  "compilerOptions": {
+    "paths": {
+      "@/*": ["./src/*"]
+    }
+  }
+}
