9ed4ab7e2b452b547e9ef5857a8ab7892c4b0ee1
TuJPan <joao.pf@estudante.iftm.edu.br>
2026-05-30 17:48:00 -0300

    Fix: Adicionando next.config e garantindo estrutura de paginas

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
-  },
+  // Configurações padrão do Next.js
 };
 
-module.exports = nextConfig;
+export default nextConfig;
